a90c057f23e9db4d1f2ffb84862b675297b373c4
Xingxin HE <xingxin.he@mail.polimi.it>
2021-12-27 20:42:03 +0800

    Fix compile error

--- codes/L02/c++/StaticArray_V6_Template/StaticArray.cpp
+++ codes/L02/c++/StaticArray_V6_Template/StaticArray.cpp
@@ -10,7 +10,8 @@ Module Name:
 
 
 Notes:
-	[Optional]
+	!!! For template class, you MUST implement them in header files !!!
+	!!! Because the linker cannot interpret the type when it is in cpp !!!
 
 
 History:
@@ -21,87 +22,3 @@ History:
 
 
 #include "StaticArray.h"
-#include <cstdio>
-#include <cstdlib>
-#include <typeinfo>
-#include <cstring>
-
-template <class T>
-StaticArray<T>::StaticArray()
-{
-	this->InitArray();
-}
-
-template <class T>
-StaticArray<T>::~StaticArray()
-{
-	this->FreeArray();
-}
-
-template <class T>
-void StaticArray<T>::InitArray()
-{
-	this->iLength = 0;
-	this->pArray = NULL;
-}
-
-template <class T>
-void StaticArray<T>::CreateArray(int size)
-{
-	this->iLength = size;
-	this->pArray = (T*)malloc(sizeof(T) * size);
-	if (this->pArray == NULL)
-	{
-		printf("No sufficient memory!");
-	}
-}
-
-template <class T>
-void StaticArray<T>::FreeArray()
-{
-	if (this->pArray != NULL)
-	{
-		free(this->pArray);
-		this->iLength = 0;
-		this->pArray = NULL;
-	}
-}
-
-template <class T>
-void StaticArray<T>::SetValue(int i, T value)
-{
-	if (this->pArray != NULL && i < this->iLength && i >= 0)
-	{
-		this->pArray[i] = value;
-	}
-	else
-	{
-		printf("The index or array is not OK.");
-	}
-}
-
-template <class T>
-void StaticArray<T>::PrintArray()
-{
-	if (this->pArray != NULL && this->iLength != 0)
-	{
-		for (int i = 0; i < this->iLength; i++)
-		{
-			char const *typen = typeid(T).name();
-			if( !strcmp(typen, "double"))
-			{
-				printf("%s: %lf ", typen, pArray[i]);
-			}
-			else if (!strcmp(typen, "int"))
-			{
-				printf("%s: %d ", typen, pArray[i]);
-			}
-			
-			printf("\n");
-		}
-	}
-	else
-	{
-		printf("The array is NULL.");
-	}
-}
--- codes/L02/c++/StaticArray_V6_Template/StaticArray.h
+++ codes/L02/c++/StaticArray_V6_Template/StaticArray.h
@@ -18,6 +18,11 @@ History:
 
 
 \************************************************************************/
+#include <cstdio>
+#include <cstdlib>
+#include <typeinfo>
+#include <cstring>
+
 
 
 template <class T>
@@ -39,5 +44,84 @@ public:
 
 
 
+template <class T>
+StaticArray<T>::StaticArray()
+{
+	this->InitArray();
+}
+
+template <class T>
+StaticArray<T>::~StaticArray()
+{
+	this->FreeArray();
+}
+
+template <class T>
+void StaticArray<T>::InitArray()
+{
+	this->iLength = 0;
+	this->pArray = NULL;
+}
+
+template <class T>
+void StaticArray<T>::CreateArray(int size)
+{
+	this->iLength = size;
+	this->pArray = (T*)malloc(sizeof(T) * size);
+	if (this->pArray == NULL)
+	{
+		printf("No sufficient memory!");
+	}
+}
+
+template <class T>
+void StaticArray<T>::FreeArray()
+{
+	if (this->pArray != NULL)
+	{
+		free(this->pArray);
+		this->iLength = 0;
+		this->pArray = NULL;
+	}
+}
+
+template <class T>
+void StaticArray<T>::SetValue(int i, T value)
+{
+	if (this->pArray != NULL && i < this->iLength && i >= 0)
+	{
+		this->pArray[i] = value;
+	}
+	else
+	{
+		printf("The index or array is not OK.");
+	}
+}
+
+template <class T>
+void StaticArray<T>::PrintArray()
+{
+	if (this->pArray != NULL && this->iLength != 0)
+	{
+		for (int i = 0; i < this->iLength; i++)
+		{
+			char const *typen = typeid(T).name();
+			if( !strcmp(typen, "double"))
+			{
+				printf("%s: %lf ", typen, pArray[i]);
+			}
+			else if (!strcmp(typen, "int"))
+			{
+				printf("%s: %d ", typen, pArray[i]);
+			}
+			
+			printf("\n");
+		}
+	}
+	else
+	{
+		printf("The array is NULL.");
+	}
+}
 
 
