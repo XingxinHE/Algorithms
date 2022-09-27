1b7fda47066cf17a641dc0deb09e014a89fea1c7
Xingxin HE <xingxin.he@mail.polimi.it>
2022-09-27 10:21:45 +0800

    code skeleton of percolation

---
+++ codes/percolation/Percolation.java
@@ -0,0 +1,43 @@
+/* *****************************************************************************
+ *  Name:              Xingxin HE
+ *  Coursera User ID:  5d0b58316c0b6c4bc2eec53c6779fe0a
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 1;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ codes/percolation/PercolationStats.java
@@ -0,0 +1,39 @@
+/* *****************************************************************************
+ *  Name:              Xingxin HE
+ *  Coursera User ID:  5d0b58316c0b6c4bc2eec53c6779fe0a
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 0.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 0.0;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 0.0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 0.0;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        return;
+    }
+
+}
